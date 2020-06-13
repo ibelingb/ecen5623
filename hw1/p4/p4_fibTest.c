a12de42c30da9a05355cb013a83dde7fa772815a
Brian Ibeling <bibeling@gmail.com>
2020-06-12 22:30:20 -0600

    Adding config of CPU Affinity as discussed in class. Results are much more consistent, but not at the accuracy to mirror a RTOS.

--- hw1/p4/p4_fibTest.c
+++ hw1/p4/p4_fibTest.c
@@ -14,17 +14,18 @@
  *  - http://ecee.colorado.edu/~ecen5623/ecen/ex/Linux/code/sequencer/lab1.c
  *  - http://ecee.colorado.edu/~ecen5623/ecen/ex/Linux/example-3/testdigest.c
  *  - https://linux.die.net/man/3/pthread_attr_init
- *  - 
- *  - 
- *  - 
- *  - 
- *  - 
+ *  - https://man7.org/linux/man-pages/man3/pthread_attr_setaffinity_np.3.html
+ *  - https://computing.llnl.gov/tutorials/pthreads/man/pthread_setaffinity_np.txt
+ *  - https://man7.org/linux/man-pages/man2/getpid.2.html
 */
 /*----------------------------------------------------------------*/
+#define _GNU_SOURCE /* Needed for CPU_SET() */
+
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sched.h>
+#include <unistd.h>
 
 #define NUM_THREADS 2
 #define SCHED_POLICY SCHED_FIFO
@@ -53,10 +54,12 @@ int fibonacci(int num);
 void setSchedPolicyPriority() {
     int maxPriority = 0;
 
+    /* Set Scheduler Policy to FIFO */
     pthread_attr_init(&schedAttr);
     pthread_attr_setinheritsched(&schedAttr, PTHREAD_EXPLICIT_SCHED);
     pthread_attr_setschedpolicy(&schedAttr, SCHED_POLICY);
 
+    /* Set Sched priority to max value */
     maxPriority = sched_get_priority_max(SCHED_POLICY);
     schedParam.sched_priority = maxPriority;
     sched_setscheduler(getpid(), SCHED_POLICY, &schedParam);
@@ -94,9 +97,14 @@ int main (int argc, char *argv[])
     int fib20Result = 0;
     struct timespec rtclk_fib10_stop_time = {0, 0};
     struct timespec rtclk_fib20_stop_time = {0, 0};
+    cpu_set_t cpuSet;
 
-    /* Set schedule policy */
+    /* Set schedule policy and priority */
     setSchedPolicyPriority();
+
+    /* Set program affinity */
+    CPU_SET(2, &cpuSet);
+    pthread_attr_setaffinity_np(&schedAttr, sizeof(cpu_set_t), &cpuSet);
 
    /* Populate threadParams variable */
    threadParams[0].fibSequenceNum = 10;
@@ -123,7 +131,7 @@ int main (int argc, char *argv[])
         pthread_join(threads[i], NULL);
     }
 
-    /* Print compute times captured */
+    /* Print results and compute times captured */
     printf("Fib10 Result: %d\nFib10 Compute time: {Seconds = %ld | nanoseconds = %ld}\n", 
             *(threadParams[0].fibSequenceResult),
             (rtclk_fib10_stop_time.tv_sec - rtclk_start_time.tv_sec), 
