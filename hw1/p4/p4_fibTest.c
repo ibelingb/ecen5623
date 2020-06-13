5128f0fa808de04eafdc34c5bcb5104e84630c4a
Brian Ibeling <bibeling@gmail.com>
2020-06-12 21:36:11 -0600

    Working pthread with fib sequence.

--- hw1/p4/p4_fibTest.c
+++ hw1/p4/p4_fibTest.c
@@ -27,9 +27,6 @@
 typedef struct
 {
     int fibSequenceNum;
-    int fibSequenceMax;
-    int fibSequenceN1;
-    int fibSequenceN2;
     int *fibSequenceResult;
     struct timespec *stopClkTime;
 } threadParams_t;
@@ -38,53 +35,50 @@ pthread_t threads[NUM_THREADS];
 threadParams_t threadParams[NUM_THREADS];
 
 static struct timespec rtclk_start_time      = {0, 0};
+static struct timespec rtclk_fib10_stop_time = {0, 0};
+static struct timespec rtclk_fib20_stop_time = {0, 0};
+static int fib10Result = 0;
+static int fib20Result = 0;
+
+int fibonacci(int num);
 
 /*----------------------------------------------------------------*/
-void fibonacci(void *threadp) {
-    threadParams_t *threadParams = (threadParams_t *)threadp;
+int fibonacci(int num) {
+    int result = 0;
 
     /* Compute Fibonacci Sequence */
-    /*
-    if(threadParams->fibSequenceNum == 0) {
-        (*threadParams->fibSequenceResult) = 0;
-        threadParams->fibSequenceN2 = 0;
-    }
-    else if(threadParams->fibSequenceNum == 1) {
-        (*threadParams->fibSequenceResult) = 1;
-        threadParams->fibSequenceN1 = 1;
+    if(num == 0 || num == 1) {
+        result = num;
     } else {
-        (*threadParams->fibSequenceResult) = (threadParams->fibSequenceN1 + threadParams->fibSequenceN2);
-        threadParams->fibSequenceN2 = threadParams->fibSequenceN1;
-        threadParams->fibSequenceN1 = (*threadParams->fibSequenceResult);
+        result = (fibonacci(num-1) + fibonacci(num-2));
     }
-    fibonacci(threadParams);
-    */
+
+    return result;
+}
+/*----------------------------------------------------------------*/
+void fibonacciThread(void *threadp) {
+    threadParams_t *threadParams = (threadParams_t *)threadp;
+    
+    /* Compute Fibonacci Sequence */
+    *(threadParams->fibSequenceResult) = fibonacci(threadParams->fibSequenceNum);
 
     /* Capture computation end time */
-    if(threadParams->fibSequenceNum == 0){
-        clock_gettime(CLOCK_REALTIME, threadParams->stopClkTime);
-    }
+    clock_gettime(CLOCK_REALTIME, threadParams->stopClkTime);
 }
 
 /*----------------------------------------------------------------*/
 int main (int argc, char *argv[])
 {
     int i = 0;
-    int fib10Result = 0;
-    int fib20Result = 0;
-    struct timespec rtclk_fib10_stop_time = {0, 0};
-    struct timespec rtclk_fib20_stop_time = {0, 0};
 
    /* Capture time of program start */
    clock_gettime(CLOCK_REALTIME, &rtclk_start_time);
 
    /* Populate threadParams variable */
-   threadParams[0].fibSequenceNum = 0;
-   threadParams[0].fibSequenceMax = 10;
+   threadParams[0].fibSequenceNum = 10;
    threadParams[0].fibSequenceResult = &fib10Result;
    threadParams[0].stopClkTime = &rtclk_fib10_stop_time;
-   threadParams[1].fibSequenceNum = 0;
-   threadParams[1].fibSequenceMax = 20;
+   threadParams[1].fibSequenceNum = 20;
    threadParams[1].fibSequenceResult = &fib20Result;
    threadParams[1].stopClkTime = &rtclk_fib20_stop_time;
 
@@ -92,7 +86,7 @@ int main (int argc, char *argv[])
    for(i=0; i<NUM_THREADS; i++) {
        pthread_create(&threads[i],   // pointer to thread descriptor
                       (void *)0,     // use default attributes
-                      fibonacci,     // thread function entry point
+                      fibonacciThread,     // thread function entry point
                       (void *)&(threadParams[i]) // parameters to pass in
                       );
    }
@@ -103,10 +97,12 @@ int main (int argc, char *argv[])
     }
 
     /* Print compute times captured */
-    printf("Fib10 Compute time: {Seconds = %ld | nanoseconds = %ld}\n",  
+    printf("Fib10 Result: %d\nFib10 Compute time: {Seconds = %ld | nanoseconds = %ld}\n", 
+            *(threadParams[0].fibSequenceResult),
             (rtclk_fib10_stop_time.tv_sec - rtclk_start_time.tv_sec), 
             (rtclk_fib10_stop_time.tv_nsec - rtclk_start_time.tv_nsec));
-    printf("Fib20 Compute time: {Seconds = %ld | nanoseconds = %ld}\n",  
+    printf("Fib20Result: %d\nFib20 Compute time: {Seconds = %ld | nanoseconds = %ld}\n",  
+            *(threadParams[1].fibSequenceResult),
             (rtclk_fib20_stop_time.tv_sec - rtclk_start_time.tv_sec), 
             (rtclk_fib20_stop_time.tv_nsec - rtclk_start_time.tv_nsec));
 
